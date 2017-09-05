7a87925e5f7804aaf6884989ce49ce8e70615c6e
si-bbg <babak_91@hotmail.com>
2017-09-05 00:41:48 -0400

    Constructor + states for class

--- app/containers/Speaker/SpeakerContainer.js
+++ app/containers/Speaker/SpeakerContainer.js
@@ -3,9 +3,28 @@ import createReactClass from 'create-react-class'
 import { Speaker } from 'components'
 
 const SpeakerContainer = createReactClass({
+  constructor() {
+    super();
+    this.state = {
+      name: null,
+      photo: null,
+      title: null,
+      tags: [],
+    }
+  }
+
+  viewProfile() {
+  }
+
   render () {
     return (
-      <Speaker />
+      <Speaker
+        name={this.state.name}
+        photo={this.state.photo}
+        title={this.state.title}
+        tags={this.state.tags}
+        viewProfile={() => this.viewProfile()}
+      />
     )
   }
 })
