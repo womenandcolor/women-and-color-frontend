e462f7bb2637930ca928f6c7947a5082325772be
re-mint <babak.eric@gmail.com>
2017-10-10 19:26:02 -0400

    Grid system set up

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -1,15 +1,21 @@
 import React, { PropTypes } from 'react'
 import { container, title } from './styles.css'
-import './styles.css'
+// import './styles.css'
 
 /* <h1 className={title}>{'Women & Color'}</h1> */
 
 export default function Home () {
   return (
-    <div className={container}>
+    <div className="container">
 
-      <div className="content">
-        <div className="content-titles">{'Speakers in Toronto for all topics'}</div>
+      <div className="row">
+          <div className="col-lg-3">
+            <button type="button" className="btn btn-success">Success</button>
+          </div>
+
+          <div className="col-lg-9">
+            <div className="content-titles">{'Speakers in Toronto for all topics'}</div>
+          </div>
       </div>
     </div>
   )
