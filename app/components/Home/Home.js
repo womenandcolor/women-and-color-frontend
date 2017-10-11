173eb18067a195dc2accf78a6217845abc187206
re-mint <babak.eric@gmail.com>
2017-10-11 01:17:13 -0400

    sbar

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react'
-import { container, title } from './styles.css'
-// import './styles.css'
+// import { container, title } from './styles.css'
+import css from './styles.css'
 
 /* <h1 className={title}>{'Women & Color'}</h1> */
 
@@ -11,6 +11,23 @@ export default function Home () {
       <div className="row">
           <div className="col-lg-3">
             <button type="button" className="btn btn-success">Success</button>
+
+            <h2 className={css.sidebarTitles}>CITY</h2>
+              <ul className="">
+                <li className={css.sidebarObject}>all cities</li>
+                <li className={css.sidebarObject}>montreal</li>
+                <li className={css.sidebarObject}>ottawa</li>
+                <li className={css.sidebarObjectSelected}>toronto</li>
+                <li className={css.sidebarObject}>vancouver</li>
+              </ul>
+
+            <div className={css.sidebarTitles}>SELF-IDENTITY</div>
+              <ul className="">
+                <li className={css.sidebarObjectSelected}>all speakers</li>
+                <li className={css.sidebarObject}>woman</li>
+                <li className={css.sidebarObject}>woman of color</li>
+                <li className={css.sidebarObject}>person of color</li>
+              </ul>
           </div>
 
           <div className="col-lg-9">
