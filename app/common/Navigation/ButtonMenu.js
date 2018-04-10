5e83374c9afe07aaf6b24155fa31f9ce5989619c
Sharon <sharon.peishan.kennedy@gmail.com>
2018-04-10 03:05:30 -0400

    use react router for edit profile link in navigation button

--- app/common/Navigation/ButtonMenu.js
+++ app/common/Navigation/ButtonMenu.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import StyledButton from 'appCommon/StyledButton';
 
-const ButtonMenu = (props) => {
+const ButtonMenu = props => {
   return (
     <div>
-      {
-        props.menuItems.map(item => (
-          <StyledButton key={item.slug} color={item.color} href={item.slug}>{item.title}</StyledButton>
-        ))
-      }
+      {props.menuItems.map(item => {
+        const link = item.slug.startsWith('/accounts')
+          ? item.slug
+          : `/#${item.slug}`;
+        return (
+          <StyledButton key={item.slug} color={item.color} href={link}>
+            {item.title}
+          </StyledButton>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default ButtonMenu;
