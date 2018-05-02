b757ede23a7781808b848d69e5d04b3c130e281d
Sharon <sharon.peishan.kennedy@gmail.com>
2018-05-02 16:12:59 -0400

    make big footer look like mockup

--- app/common/Footer/FullFooter.js
+++ app/common/Footer/FullFooter.js
@@ -2,13 +2,57 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Grid from 'material-ui/Grid';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import faGithub from '@fortawesome/fontawesome-free-brands/faGithub'
+import faTwitter from '@fortawesome/fontawesome-free-brands/faTwitter'
+import faInstagram from '@fortawesome/fontawesome-free-brands/faInstagram'
 
 // APP
+import css from './styles.css';
+import Tribalscale from 'svg-react-loader?name=Tribalscale!../../assets/Tribalscale.svg';
+import Wealthsimple from 'svg-react-loader?name=Wealthsimple!../../assets/Wealthsimple.svg';
 
 const FullFooter = () => {
   return (
-    <div>
-      <Grid container justify="center">
+    <footer className={css.footer}>
+      <Grid container justify="center" className={`${css.backgroundSecondary}`} spacing={0}>
+        <Grid item xs={12} md={9}>
+          <Grid container justify="center" alignItems="center" className={css.partnerLogos}>
+            <a href="https://www.wealthsimple.com">
+              <Wealthsimple />
+            </a>
+            <a href="http://www.tribalscale.com">
+              <Tribalscale />
+            </a>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid container justify="center" className={`${css.footerRow} ${css.backgroundPrimary}`} spacing={0}>
+        <Grid item xs={12} md={9}>
+          <Grid container justify="space-between">
+            <Grid item>
+              <a href="#">About us</a>
+              <a href="#">Special Thanks</a>
+              <a href="#">Our Partners</a>
+              <a href="#">Contact Us</a>
+            </Grid>
+            <Grid item>
+              <a href="https://twitter.com/womenandcolor" target='_blank'>
+                <FontAwesomeIcon icon={faTwitter} size='lg' />
+              </a>
+              <a href="https://github.com/CivicTechTO/women-and-color-frontend" target='_blank'>
+                <FontAwesomeIcon icon={faGithub} size='lg' />
+              </a>
+              <a href="https://www.instagram.com/womenandcolor/" target='_blank'>
+                <FontAwesomeIcon icon={faInstagram} size='lg' />
+              </a>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid container justify="center" className={`${css.footerRow} ${css.backgroundPrimaryDark}`} spacing={0}>
         <Grid item xs={12} md={9}>
           <Grid container justify="space-between">
             <Grid item>
@@ -21,24 +65,7 @@ const FullFooter = () => {
           </Grid>
         </Grid>
       </Grid>
-      <Grid container justify="center">
-        <Grid item xs={12} md={9}>
-          <Grid container justify="space-between">
-            <Grid item>
-              <a href="#">About us</a>
-              <a href="#">Special Thanks</a>
-              <a href="#">Our Partners</a>
-              <a href="#">Contact Us</a>
-            </Grid>
-            <Grid item>
-              <a href="#">
-
-              </a>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
-    </div>
+    </footer>
   )
 }
 
