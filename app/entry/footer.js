d898233b564a69173e92affd350534dd10707fea
Sharon <sharon.peishan.kennedy@gmail.com>
2018-05-14 03:39:35 -0400

    get rid of theme on mini footer

--- app/entry/footer.js
+++ app/entry/footer.js
@@ -1,26 +1,12 @@
 // NPM
 import ReactDOM from 'react-dom'
 import React from 'react'
-import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 
 // App
 import MiniFooter from 'appCommon/Footer/MiniFooter';
-import { container } from './styles.css';
 
-const theme = createMuiTheme({
-  typography: {
-    fontFamily: "\"Open Sans\", \"Helvetica\", \"Arial\", sans-serif"
-  }
-});
-
-
-const MainContainer = (props) => (
-  <MuiThemeProvider theme={theme}>
-    <MiniFooter />
-  </MuiThemeProvider>
-)
 
 ReactDOM.render(
-  <MainContainer />,
+  <MiniFooter />,
   document.getElementById('footer-container')
 )
