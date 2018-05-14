730842696c71fd33727caefe33a01b2d870a05f3
Sharon <sharon.peishan.kennedy@gmail.com>
2018-05-14 01:29:59 -0400

    terms of use, code of conduct, and custom 404 page

---
+++ app/pages/Static/PageNotFound.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Grid from 'material-ui/Grid';
+import { Link } from 'react-router-dom';
+
+import css from './styles.css'
+
+const CodeOfConduct = () => {
+  return(
+    <Grid container justify="center">
+      <Grid item xs={11} md={9}>
+        <header className={css.header}>
+          <h1>There's nothing here :(</h1>
+          <p>The URL was not found. Let's go back to the <Link to='/'>home page.</Link></p>
+        </header>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default CodeOfConduct;
