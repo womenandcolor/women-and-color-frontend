748455f89d8dfdd33b5b33a895f0dda910101c3c
Mohammed Asaduallah <9420878+heymosef@users.noreply.github.com>
2018-09-14 23:01:12 -0400

    Removed link for Slack group

--- app/pages/Static/AboutUs.jsx
+++ app/pages/Static/AboutUs.jsx
@@ -52,7 +52,7 @@ const AboutUs = (props) => {
             <Grid item xs={12} sm={10} md={6}>
               <p>Women and Color is an online community of subject matter experts who identify as women and/or people of color.</p>
               <p>Located in cities across Canada and the United States, each of our subject matter experts is available for speaking opportunities at tech-related events.</p>
-              <p>Interested in helping us build a more inclusive tech ecosystem? Send us an email at <span className={props.classes.strong}>hello@womenandcolor.com</span> or <a href="https://join.slack.com/t/womenandcolor/shared_invite/enQtNDE5MDA4MjU4MTQ5LTY4NGYxMjAyODJiODhkMDU3NjEwZmQyYWMzMWQ1ZjAwNjYzYmY5ZmIxNzRkMDM1ZjUwOTc5MjQ0NmFkMzNjMTY" target="_blank" className={props.classes.strong}>join us on Slack</a>.</p>
+              <p>Interested in helping us build a more inclusive tech ecosystem? Send us an email at <span className={props.classes.strong}>hello@womenandcolor.com</span>.</p>
               <p>Women and Color is built with passion in Toronto, ON, and is a federally incorporated not-for-profit in Canada.</p>
             </Grid>
           </Grid>
